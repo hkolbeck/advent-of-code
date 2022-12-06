1811774e7bbb0c0e9ec0ff9744a803fd69249e09
Hannah Kolbeck <hskolbeck@gmail.com>
2022-12-05 21:19:49 -0800

    Day 6

---
+++ 2022/6/src/main.rs
@@ -0,0 +1,36 @@
+use std::collections::HashSet;
+use std::fs;
+use std::time::Instant;
+use nom::InputIter;
+
+fn main() {
+    let start = Instant::now();
+    let score = solve_part_1();
+    let end = Instant::now();
+    println!("Part 1: {} in {}us", score, end.duration_since(start).as_micros());
+
+    let start = Instant::now();
+    let score = solve_part_2();
+    let end = Instant::now();
+    println!("Part 2: {} in {}us", score, end.duration_since(start).as_micros());
+}
+
+fn solve_part_1() -> i64 {
+    solve_n(4)
+}
+
+fn solve_part_2() -> i64 {
+    solve_n(14)
+}
+
+fn solve_n(n: usize) -> i64 {
+    let input = fs::read_to_string("input.txt").unwrap();
+    for i in 0..input.len() {
+        let set: HashSet<char> = (&input[i..i+n]).iter_elements().collect();
+        if set.len() == n {
+            return (i + n) as i64;
+        }
+    }
+
+    -1
+}
