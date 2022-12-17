7b7ffe147a4fa626af9b3b579e504e8e0f47bace
Hannah Kolbeck <hskolbeck@gmail.com>
2022-12-16 16:53:34 -0800

    Day 16

---
+++ 2022/16/src/main.rs
@@ -0,0 +1,204 @@
+use std::collections::{HashMap, HashSet};
+use std::fs;
+use std::str::FromStr;
+use std::time::Instant;
+use itertools::Itertools;
+use regex::Regex;
+
+fn main() {
+    let raw_input = fs::read_to_string("input.txt").unwrap();
+    let lines: Vec<String> = raw_input.split('\n')
+        .filter(|s| !s.is_empty())
+        .map(String::from)
+        .collect();
+    let (graph, pressures) = parse_input(lines);
+
+    // let start = Instant::now();
+    // let answer_1 = solve_1(graph.clone(), pressures.clone());
+    // let end = Instant::now();
+    // println!("Part 1: {} in {}us", answer_1, end.duration_since(start).as_micros());
+
+    let start = Instant::now();
+    let answer_2 = solve_2(graph, pressures);
+    let end = Instant::now();
+    println!("Part 2: {} in {}us", answer_2, end.duration_since(start).as_micros());
+}
+
+fn solve_1(graph: HashMap<String, Vec<String>>, pressures: HashMap<String, usize>) -> usize {
+    let start = String::from("AA");
+    let distances = distances(&graph);
+    let has_pressure = pressures.iter()
+        .filter(|(_, p)| p > &&0)
+        .map(|(v, _)| v)
+        .collect();
+
+    search(0, 30, &start, &has_pressure, &pressures, &distances, &HashSet::new())
+}
+
+fn solve_2(graph: HashMap<String, Vec<String>>, pressures: HashMap<String, usize>) -> usize {
+    let start = String::from("AA");
+    let distances = distances(&graph);
+    let has_pressure: HashSet<String> = pressures.iter()
+        .filter(|(_, p)| p > &&0)
+        .map(|(v, _)| v.clone())
+        .collect();
+    let mut c = 0;
+    let mut max = 0;
+    has_pressure.iter().combinations(8)
+        .for_each(|you| {
+            let elephant: Vec<&String> = has_pressure.iter()
+                .filter(|v| !you.contains(v))
+                .collect();
+
+            let you_released = search(
+                0,
+                26,
+                &start,
+                &you,
+                &pressures,
+                &distances,
+                &HashSet::new()
+            );
+            let ele_released = search(
+                0,
+                26,
+                &start,
+                &elephant,
+                &pressures,
+                &distances,
+                &HashSet::new()
+            );
+
+            c += 1;
+            if c % 25 == 0 {
+                println!("{}, max: {}", c, max);
+            }
+
+            max = max.max(ele_released + you_released)
+        });
+
+    max
+}
+
+fn search(
+    minute: usize,
+    max_minute: usize,
+    at: &String,
+    have_pressure: &Vec<&String>,
+    pressures: &HashMap<String, usize>,
+    distances: &HashMap<(&String, &String), usize>,
+    open: &HashSet<&String>,
+) -> usize {
+    let mut open = open.clone();
+    open.insert(at);
+
+    let pressure = pressures.get(at).unwrap();
+    let mut max = 0;
+    for next in have_pressure {
+        let dist = distances.get(&(at, next)).unwrap();
+        if !open.contains(next) && minute + dist < max_minute {
+            let result = search(
+                minute + dist + 1,
+                max_minute,
+                next,
+                have_pressure,
+                pressures,
+                distances,
+                &open,
+            );
+
+            max = max.max(result)
+        }
+    }
+
+    max + pressure * (max_minute - minute)
+}
+// fn run_path(
+//     start: &String,
+//     path: &Vec<&String>,
+//     pressures: &HashMap<String, usize>,
+//     distances: &HashMap<(&String, &String), usize>,
+// ) -> (usize, String) {
+//     let mut max = 0;
+//     let mut max_path = String::from("");
+//     path.iter().permutations(path.len())
+//         .map(|mut p| {
+//             p.insert(0, &start);
+//             p
+//         })
+//         .for_each(|path| {
+//             let mut minute = 0;
+//             let mut released = 0;
+//
+//             path.windows(2).for_each(|step| {
+//                 let dist = *distances.get(&(*(step[0]), step[1])).unwrap();
+//                 let pressure = *pressures.get(*step[1]).unwrap();
+//                 if minute + dist + 1 >= 26 {
+//                     ControlFlow::<(), ()>::Break(());
+//                 } else {
+//                     released += pressure * (26 - minute - dist - 1);
+//                     minute += dist + 1;
+//                 }
+//             });
+//
+//             if released > max {
+//                 max_path = format!("{:?}", path);
+//                 max = released;
+//             }
+//         });
+//
+//     (max, max_path)
+// }
+
+fn distances(graph: &HashMap<String, Vec<String>>) -> HashMap<(&String, &String), usize> {
+    let mut node_dists = HashMap::new();
+    for start in graph.keys() {
+        let mut queue: HashSet<&String> = HashSet::new();
+        let mut dists = HashMap::new();
+
+        for node in graph.keys() {
+            dists.insert(node, usize::MAX);
+            queue.insert(node);
+        }
+        dists.insert(start, 0);
+
+        while !queue.is_empty() {
+            let node = queue.iter()
+                .min_by(|v1, v2| dists.get(**v1).unwrap().cmp(dists.get(**v2).unwrap()))
+                .unwrap().clone();
+            queue.remove(node);
+
+            for neighbor in graph.get(node).unwrap() {
+                if queue.contains(neighbor) {
+                    let maybe = dists.get(node).unwrap() + 1;
+                    if &maybe < dists.get(neighbor).unwrap() {
+                        dists.insert(neighbor, maybe);
+                    }
+                }
+            }
+        }
+
+        for (other_node, dist) in dists {
+            node_dists.insert((start, other_node), dist);
+        }
+    }
+
+    node_dists
+}
+
+fn parse_input(lines: Vec<String>) -> (HashMap<String, Vec<String>>, HashMap<String, usize>) {
+    let regex = Regex::new("Valve (..) has flow rate=(\\d+); tunnels? leads? to valves? (.*)").unwrap();
+    let mut graph = HashMap::new();
+    let mut pressures = HashMap::new();
+    for line in lines {
+        let captures = regex.captures(line.as_str()).unwrap();
+        let name = String::from(captures.get(1).unwrap().as_str());
+        let flow_rate = usize::from_str(captures.get(2).unwrap().as_str()).unwrap();
+        let connected = captures.get(3).unwrap().as_str().split(", ").map(String::from).collect();
+
+        graph.insert(name.clone(), connected);
+        pressures.insert(name, flow_rate);
+    }
+
+    (graph, pressures)
+}
