86537f1a109b9027978576d47981a81cd09dbd2b
Hannah Kolbeck <hskolbeck@gmail.com>
2022-12-20 20:26:30 -0800

    Day 20 - Incomplete

--- 2022/20/src/main.rs
+++ 2022/20/src/main.rs
@@ -1,5 +1,5 @@
-use std::collections::HashMap;
 use std::fs;
+use std::str::FromStr;
 use std::time::Instant;
 
 fn main() {
@@ -21,14 +21,86 @@ fn main() {
     println!("Part 2: {} in {}us", answer_2, end.duration_since(start).as_micros());
 }
 
-fn solve_1(map: HashMap<(), usize>) -> usize {
-    todo!()
+fn solve_1(ring: Vec<i64>) -> i64 {
+    let mut idx_ring = vec![];
+    for idx in 0..ring.len() {
+        idx_ring.push((idx, ring[idx]));
+    }
+
+    let mut buf = RingBuffer {
+        buf: idx_ring
+    };
+
+    for orig_idx in 0..ring.len() {
+        // println!("{}: {:?}", orig_idx, buf);
+        let cur_idx = buf.find_idx(orig_idx);
+        buf.shift(1, cur_idx);
+    }
+
+    // 1, 2, -3, 4, 0, 3, -2
+    // println!("{:?}", buf);
+
+    let zero_idx = buf.find_0();
+    println!("{}, {}, {}",     buf.nth(zero_idx, 1000), buf.nth(zero_idx, 2000), buf.nth(zero_idx, 3000));
+    buf.nth(zero_idx, 1000) + buf.nth(zero_idx, 2000) + buf.nth(zero_idx, 3000)
 }
 
-fn solve_2(_map: HashMap<(), usize>) -> usize {
+fn solve_2(_ring: Vec<i64>) -> usize {
     0
 }
 
-fn parse_input(lines: Vec<String>) -> HashMap<(), usize> {
-    todo!()
+#[derive(Debug)]
+struct RingBuffer {
+    buf: Vec<(usize, i64)>,
+}
+
+impl RingBuffer {
+    pub fn find_idx(&self, orig_idx: usize) -> usize {
+        for idx in 0..self.buf.len() {
+            let (oi, _) = self.buf[idx];
+            if oi == orig_idx {
+                return idx;
+            }
+        }
+
+        panic!("Not found!")
+    }
+
+    pub fn shift(&mut self, key: i64, idx: usize) {
+        let (orig_idx, num) = self.buf[idx];
+        let shift = (key * num) % self.buf.len() as i64;
+        // println!("Shift: {}", shift);
+        self.buf.remove(idx);
+        if shift < 0 {
+            let shift = (self.buf.len() as i64 + shift) as usize;
+            self.buf.rotate_left(shift);
+        } else if shift > 0 {
+            self.buf.rotate_left(shift as usize);
+        }
+        self.buf.insert(idx, (orig_idx, num));
+    }
+
+    fn find_0(&self) -> usize {
+        let mut zero_loc = usize::MAX;
+        for idx in 0..self.buf.len() {
+            if self.buf[idx].1 == 0 {
+                zero_loc = idx;
+                break;
+            }
+        };
+
+        if zero_loc == usize::MAX {
+            panic!("No zero!");
+        }
+
+        zero_loc
+    }
+
+    fn nth(&self, base: usize, n: usize) -> i64 {
+        self.buf[(base + n) % self.buf.len()].1
+    }
+}
+
+fn parse_input(lines: Vec<String>) -> Vec<i64> {
+    lines.iter().map(|l| i64::from_str(l.as_str()).unwrap()).collect()
 }
